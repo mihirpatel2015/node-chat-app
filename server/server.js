814df04eef55ac2da103e56638722fae1771a768
mihirpatel2015 <mihirpatel2015@gmail.com>
2020-02-28 19:55:53 +0530

    add new message and create message event

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected.')
 
+    socket.emit('newMessage', {
+        from: 'mihirpatel2015@gmail.com',
+        text: 'Hey, what is going on.',
+        creatAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createdMessage', message)
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected.');
     })
